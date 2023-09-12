a825b88497d46314b20f5b18a7d2b303d948b416
Ricardo Melo <127363426+ricardojs122@users.noreply.github.com>
2023-09-11 23:49:03 -0300

    Melhorando e replicando as técnicas aprendidas ao longo do curso.
    Buscando sempre o melhor.

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,24 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent implements OnInit {
+export class SmallCardComponent {
 
   @Input()
-  photoCover:string = ""
+  photoCover: string = "";
 
   @Input()
-  cardTitle:string = ""
+  cardTitle: string = "";
 
   @Input()
-  Id:string="0"
+  Id: string = "0";
 
   constructor() { }
 
-  ngOnInit(): void {
+  getTitleColor(): string {
+    if (this.Id === "1") {
+      return "red";
+    } else if (this.Id === "2") {
+      return "#F1F";
+    } else if (this.Id === "3") {
+      return "#6b53b5ff";
+    } else if (this.Id === "4") {
+      return "#1d9df3";
+    } else if (this.Id === "5") {
+      return "red";
+    } else {
+      return "black";
+    }
   }
-
 }
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -5,10 +5,35 @@ export const dataFake = [
     "description": "marvel anuncia um novo filme do homem de ferro, confira",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
+
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "title": "Novas Séries anunciadas no Disney +",
+    "description": "Novas séries foram anunciadas no Disney +",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra será lançado em breve.",
+    "description": `Pantera Negra: Legado" é o próximo filme da Marvel que continua a história de Wakanda após os eventos de "Vingadores: Ultimato". O filme segue Shuri enquanto ela lidera Wakanda e enfrenta desafios para proteger o reino e o legado de seu irmão, T'Challa. Oferece ação, intriga política e uma homenagem a Chadwick Boseman, mantendo o espírito do Pantera Negra. Wakanda para sempre!`,
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor ",
+    "description": "Os fãs de quadrinhos e filmes de super-heróis agora terão que aguardar ansiosamente por mais detalhes, mas uma coisa é certa: os X-Men estão se preparando para mais uma emocionante aventura que promete elevar o gênero a novos patamares. ",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  },
+  {
+    "id":"5",
+    "title": "Spider-Man 2 Sairá para PS5 ",
+    "description": "O jogo está muito proximo de sair e já está confirmado para o famoso console da SONY. ",
+    "photoCover":"https://image.api.playstation.com/vulcan/ap/rnd/202306/1219/1c7b75d8ed9271516546560d219ad0b22ee0a263b4537bd8.png"
   }
-]
+
+
+
+];
+
